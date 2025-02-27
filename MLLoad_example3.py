b4c5d11f9f463cb1622a91f6781db7d790a7b4da
balajitarkasband <127940120+balajitarkasband@users.noreply.github.com>
2025-02-27 14:16:05 +0530

    Delete MLLoad_example3.py

--- MLLoad_example3.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd 
-import pymysql
-db = pymysql
-db = pymysql.connect(host = 'localhost',passwd = '',user = 'root', db='Elearning')
-cursor = db.cursor()
-sql = "select * from Users"
-df = pd.read_sql_query(sql,db)
-print(df.head(2))
